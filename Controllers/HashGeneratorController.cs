2d1f03f7b6457e66109c12eaf6a9e76ab7564092
Akash Verma <114301491+AkashVerma2803@users.noreply.github.com>
2025-12-03 15:31:53 +0530

    Add files via upload

---
+++ Controllers/HashGeneratorController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Banking_CapStone.Service;
+
+namespace Banking_CapStone.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HashGeneratorController : ControllerBase
+    {
+        private readonly PasswordHasher _passwordHasher;
+
+        public HashGeneratorController()
+        {
+            _passwordHasher = new PasswordHasher();
+        }
+
+        [HttpGet("generate")]
+        public IActionResult GenerateHashes()
+        {
+            var hashes = new
+            {
+                SuperAdminHash = _passwordHasher.HashPassword("Admin@123"),
+                BankUserHash = _passwordHasher.HashPassword("Bank@123"),
+                ClientHash = _passwordHasher.HashPassword("Client@123")
+            };
+
+            return Ok(hashes);
+        }
+    }
+}
---
+++ Data/DatabaseSeeder.cs
@@ -0,0 +1,14 @@
+﻿using Banking_CapStone.Service;
+
+namespace Banking_CapStone.Data
+{
+    public static class DatabaseSeeder
+    {
+        private static readonly PasswordHasher _passwordHasher = new PasswordHasher();
+
+        public static readonly string SuperAdminPasswordHash = "100000.Z99C/OvzNeLO3OA0GiirHQ==.vzyX10fSzZGjQQb5cAv1ONnVW52HkhOWHMNhLbM2udI=";
+        public static readonly string BankUserPasswordHash = "100000.Rhxk8Ie+cgBtroK88pKBhw==.cyLg+f5S9PoO+4S/hLqz2USvhVhzO2bah9ETgK6A6nE=";
+        public static readonly string ClientPasswordHash = "100000.e4t4587m+1yRkgJsD0QeEg==.ZO93gkE38xLxUA3FmBul8dY5eF5o7ZFSE6R1PkIJSu4=";
+        public static readonly DateTime SeedDate = new DateTime(2024, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+    }
+}
